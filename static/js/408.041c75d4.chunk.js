674e8c2fee063cf2719e47f12801b4523b72ead5
DmytroPod <132753424+DmytroPod@users.noreply.github.com>
2023-12-12 17:51:39 +0000

    Deploying to gh-pages from @ DmytroPod/goit-react-hw-05-movies@43bfd38f204c356c430d24807deddf574e41c473 🚀

--- static/js/408.041c75d4.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[408],{408:function(e,i,t){t.r(i);var s=t(184);i.default=function(){return(0,s.jsx)("div",{children:"MoviesPage"})}}}]);
-//# sourceMappingURL=408.041c75d4.chunk.js.map
