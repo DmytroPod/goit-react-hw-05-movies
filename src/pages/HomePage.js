f595e22dfa55b6ce3b911df236f91fc5a73a5bef
DmytroPod <132753424+DmytroPod@users.noreply.github.com>
2023-12-15 00:34:10 +0200

    fix

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,14 @@
 import { useEffect, useState } from 'react';
 import { getTrending } from 'Servise/API';
 import MoviesList from 'components/MoviesList/MoviesList';
+import Loader from 'components/Loader/Loader';
 
 const HomePage = () => {
   const [trendings, setTrendings] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
+    setIsLoading(true);
     const fetchTrendings = async () => {
       try {
         const response = await getTrending();
@@ -13,12 +16,19 @@ const HomePage = () => {
         setTrendings(response);
       } catch (err) {
         console.log(err);
+      } finally {
+        setIsLoading(false);
       }
     };
     fetchTrendings();
   }, []);
 
-  return <MoviesList movies={trendings} />;
+  return (
+    <>
+      {isLoading && <Loader />}
+      <MoviesList movies={trendings} />
+    </>
+  );
 };
 
 export default HomePage;
